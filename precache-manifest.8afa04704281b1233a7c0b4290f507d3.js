687b4f39ec2db7768eb5b16d40e0ecad919ed912
punkoc <punkoco@gmail.com>
2024-12-10 20:16:05 -0300

    Updates

---
+++ precache-manifest.8afa04704281b1233a7c0b4290f507d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213ba13ea1b87bf3cf69e292ae7833df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6614528e1c646cc6f3f3",
+    "url": "/static/css/2.a10018f7.chunk.css"
+  },
+  {
+    "revision": "4c5e2bbb7b04a1d2d602",
+    "url": "/static/css/main.de70eb2a.chunk.css"
+  },
+  {
+    "revision": "6614528e1c646cc6f3f3",
+    "url": "/static/js/2.aa693586.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.aa693586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aaa29f7c55792cb6e82",
+    "url": "/static/js/3.f5a516dc.chunk.js"
+  },
+  {
+    "revision": "4c5e2bbb7b04a1d2d602",
+    "url": "/static/js/main.953c48d9.chunk.js"
+  },
+  {
+    "revision": "2719ae1dffe49686d154",
+    "url": "/static/js/runtime-main.62c1cfe4.js"
+  },
+  {
+    "revision": "7edd2a50bce097a2c89958a232b62357",
+    "url": "/static/media/fundofundo.7edd2a50.png"
+  }
+]);
